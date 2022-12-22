e18f85751522b4353f43e8e5285480eecffa7fb9
MohammadMahdiEsmayili <mahdiesm8013@gmail.com>
2022-12-22 13:48:27 +0330

    Ui-Layer
    start create ui layer

--- pages/api/login.js
+++
@@ -1,8 +0,0 @@
-import { faker } from "@faker-js/faker";
-
-export default function handler(req, res) {
-  res.status(200).json({
-    message: "You Logged-In successfully",
-    token: faker.internet.ip(),
-  });
-}
---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const sliderItems =[
+    {
+        id: 1,
+        img: "https://www.finds.ir/img/2018092679100243.jpg" ,
+        title: "Beats Pro هدفون بیتس پرو " ,
+        desc: " قابل تا شدن جهت حمل نقل آسان ارائه صدای شفاف و فرکانس‌های بسیار دقیق،ایده آل برای تدوینگران صوتی صدای بیس فوق‌العاده رسا و با کیفیت بدنه تمام آلومینیومی " ,
+        bg: "f5fafd" ,
+    },
+    {
+        id: 2,
+        img: "" ,
+        title: "Beats ProMax هدفون بیتس پرو " ,
+        desc: " قابل تا شدن جهت حمل نقل آسان ارائه صدای شفاف و فرکانس‌های بسیار دقیق،ایده آل برای تدوینگران صوتی صدای بیس فوق‌العاده رسا و با کیفیت بدنه تمام آلومینیومی " ,
+        bg: "f5fafd" ,
+    },
+    {
+        id: 3,
+        img: "" ,
+        title: "Beats ProMax هدفون بیتس پرو " ,
+        desc: " قابل تا شدن جهت حمل نقل آسان ارائه صدای شفاف و فرکانس‌های بسیار دقیق،ایده آل برای تدوینگران صوتی صدای بیس فوق‌العاده رسا و با کیفیت بدنه تمام آلومینیومی " ,
+        bg: "f5fafd" ,
+    },
+];
+
+export const categories = [
+    {
+      id: 1,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_8Yj-4yK8tOxAFBWzAyvs22SeipbtCviX3w&usqp=CAU",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://www.finds.ir/img/2018092679100243.jpg",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://www.finds.ir/img/2018092679100243.jpg",
+      title: "LIGHT JACKETS",
+    },
+  ];
